2741cb661615596f0f3fd7697b708182796bef75
yannboca <94798846+yannboca@users.noreply.github.com>
2026-06-18 20:01:27 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -53,17 +53,17 @@ const characters = {
   dreamer: {
     title: "The Dreamkeeper",
     text: "You collect memories and transform them into new worlds.",
-    img: "Images/collection1.jpeg.jpeg"
+    img: "../Images/collection1.jpeg"
   },
   storyteller: {
     title: "The Storyteller",
     text: "Every mystery becomes part of your journey.",
-    img: "Images/collection2.jpeg.jpeg"
+    img: "../Images/collection2.jpeg"
   },
   wanderer: {
     title: "The Wanderer",
     text: "Beyond every gate lies a new chapter. You never stop exploring.",
-    img: "Images/collection3.jpeg.jpeg"
+    img: "../Images/collection3.jpeg"
   }
 };
 
